43f6cd1cf548414bd673d8f3c2c5ac4a442268c0
SofiaNechytailo <134634542+SofiaNechytailo@users.noreply.github.com>
2023-07-19 22:33:42 +0200

    done

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Pair pair = new Pair<>(1, 'a');
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+    }
+}
---
+++ src/Pair.java
@@ -0,0 +1,14 @@
+public class Pair <T, E>{
+    E first;
+    E second;
+    public Pair(E first, E second){
+        this.first = first;
+        this.second = second;
+    }
+    public E getFirst() {
+        return this.first;
+    }
+    public E getSecond() {
+        return second;
+    }
+}
